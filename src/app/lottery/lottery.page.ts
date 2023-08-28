349f3f1387e6df6e4fbd14771a8aba4813c82cc2
NieRiFeng <17673376360@163.com>
2023-08-28 17:54:56 +0800

    添加基本测试动画文件

--- src/app/lottery/lottery.page.ts
+++ src/app/lottery/lottery.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -8,6 +8,62 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule],
 })
-export class LotteryPage {
+export class LotteryPage implements OnInit {
+  public daLeTou: string = '';
+  public shuangSeqiu: string = '';
   constructor() {}
+  public ngOnInit(): void {
+    this.generateNumber();
+  }
+
+  public generateNumber() {
+    console.log(1);
+    this.daLeTou = this.generateDaLeTouRandomArray();
+    this.shuangSeqiu = this.generateSSQRandomNumbers();
+  }
+
+  private generateDaLeTouRandomArray(): string {
+    const blueBalls = Array.from({ length: 35 }, (_, index) =>
+      (index + 1).toString().padStart(2, '0')
+    );
+    const yellowBalls = Array.from({ length: 12 }, (_, index) =>
+      (index + 1).toString().padStart(2, '0')
+    );
+
+    const selectedBlueBalls = [];
+    while (selectedBlueBalls.length < 5) {
+      const randomIndex = Math.floor(Math.random() * blueBalls.length);
+      const selectedBall = blueBalls.splice(randomIndex, 1)[0];
+      selectedBlueBalls.push(selectedBall);
+    }
+
+    const selectedYellowBalls = [];
+    while (selectedYellowBalls.length < 2) {
+      const randomIndex = Math.floor(Math.random() * yellowBalls.length);
+      const selectedBall = yellowBalls.splice(randomIndex, 1)[0];
+      selectedYellowBalls.push(selectedBall);
+    }
+
+    return [...selectedBlueBalls, ...selectedYellowBalls].join(',');
+  }
+
+  private generateSSQRandomNumbers(): string {
+    const redBalls = Array.from({ length: 33 }, (_, index) =>
+      (index + 1).toString().padStart(2, '0')
+    );
+    const blueBalls = Array.from({ length: 16 }, (_, index) =>
+      (index + 1).toString().padStart(2, '0')
+    );
+
+    const selectedRedBalls = [];
+    while (selectedRedBalls.length < 6) {
+      const randomIndex = Math.floor(Math.random() * redBalls.length);
+      const selectedBall = redBalls.splice(randomIndex, 1)[0];
+      selectedRedBalls.push(selectedBall);
+    }
+
+    const selectedBlueBall =
+      blueBalls[Math.floor(Math.random() * blueBalls.length)];
+    return [...selectedRedBalls, selectedBlueBall].join(',');
+  }
 }
